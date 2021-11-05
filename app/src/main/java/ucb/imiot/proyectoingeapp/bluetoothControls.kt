f814a5b4b88f38d8b671d1bb5429470285f4db69
Douke017 <douglas.mendez@ucb.edu.bo>
2021-11-05 15:00:03 -0400

    Initial commit

---
+++ app/src/main/java/ucb/imiot/proyectoingeapp/bluetoothControls.kt
@@ -0,0 +1,17 @@
+package ucb.imiot.proyectoingeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bluetooth_controls.*
+import kotlinx.android.synthetic.main.activity_physics_forms.*
+
+class bluetoothControls : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth_controls)
+
+        info_button.setOnClickListener{startActivity(Intent(applicationContext,InformationBluetoothControl::class.java))}
+
+    }
+}
---
+++ app/src/main/java/ucb/imiot/proyectoingeapp/form.kt
@@ -0,0 +1,36 @@
+package ucb.imiot.proyectoingeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class form : AppCompatActivity() {
+
+    lateinit var fisicaBtn:Button
+    lateinit var mateBtn:Button
+    lateinit var calculoBtn:Button
+    lateinit var cppBtn:Button
+    lateinit var pythonBtn:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+
+        fisicaBtn = findViewById(R.id.fisica_button)
+        fisicaBtn.setOnClickListener{startActivity(Intent(applicationContext,physicsForms::class.java))}
+
+        mateBtn = findViewById(R.id.matematicas_button)
+        mateBtn.setOnClickListener{startActivity(Intent(applicationContext,mathForms::class.java))}
+
+        calculoBtn = findViewById(R.id.calculo_button)
+        calculoBtn.setOnClickListener{startActivity(Intent(applicationContext,calculusForms::class.java))}
+
+        cppBtn = findViewById(R.id.cpp_button)
+        cppBtn.setOnClickListener{startActivity(Intent(applicationContext,cppForms::class.java))}
+
+        pythonBtn = findViewById(R.id.python_button)
+        pythonBtn.setOnClickListener{startActivity(Intent(applicationContext,pythonForms::class.java))}
+
+    }
+}
